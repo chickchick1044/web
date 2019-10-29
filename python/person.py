74a46e37405466700b58879603245c2df008ee3a
chickchick1044 <chickchick1044@naver.com>
2019-10-29 17:43:46 +0900

    191029 Django ORM, CRUD

---
+++ python/person.py
@@ -0,0 +1,25 @@
+class Person():
+    name = '사람의 고유한 속성' # 멤버 변수: 집단의 공통 속성
+    age = '출생 이후부터 삶을 마감할 때까지의 시간'
+
+    def greeting(self): # 멤버 메서드: 개개인이 할 수 있는 행동
+        print(f'{self.name}이 인사합니다. 안녕하세요')
+
+    def eating(self):
+        print(f'{self.name}은 밥을 먹고 있습니다')
+
+junwoo = Person() # 인스턴스 생성
+justin = Person()
+print(junwoo.name)
+print(junwoo.age)
+
+junwoo.name = 'junwoo'
+junwoo.age = '50'
+print(junwoo.name)
+print(junwoo.age)
+
+print(Person.name)
+print(Person.age)
+junwoo.greeting()
+
+
