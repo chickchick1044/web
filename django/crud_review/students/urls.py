6ef9fafaa53b7b6242fa27752ef024f19c2794f8
chickchick1044 <chickchick1044@naver.com>
2019-10-31 17:45:15 +0900

    191031 Django Restful

--- django/crud_review/students/urls.py
+++ django/crud_review/students/urls.py
@@ -1,13 +1,34 @@
 from django.urls import path
 from . import views
 
+app_name = 'students'
+
 urlpatterns = [
-    path('', views.index),
-    path('new/', views.new),
-    path('create/', views.create),
-    path('<int:pk>/', views.detail),
-    path('<int:pk>/delete/', views.delete),
-    path('<int:pk>/edit/', views.edit),
-    path('<int:pk>/update/', views.update),
+    path('', views.index, name='index'), # GET /students/
+    path('new/', views.new, name='new'), # GET /students/new/
+    # path('create/', views.create, name='create'), => 주소자체에 표현이 들어가서X (자원만 포함해야), 해결: GET과 POST를 합친다
+    path('<int:pk>/', views.detail, name='detail'), # GET /students/1/
+    path('<int:pk>/edit/', views.edit, name='edit'), # GET, POST /student/1/edit
+    # path('<int:pk>/update/', views.update, name='update'), # POST /students/1/update(X)
+    path('<int:pk>/delete/', views.delete, name='delete'), # POST /students/1/delete => Restful하진 않지만, 장고는 delete를 지원하지 않음
 
 ]
+
+# URL Name
+# path('주소/', views.함수, name='이름')
+# {% url '이름'%} => /students/이름/가 자동으로 생성
+# [장점]
+# 1. 주소의 변경이 필요할 때, urls.py에서만 수정해주면 됨
+# 2. 마지막 '/'를 빼먹는 실수를 차단할 수 있음
+
+# App Name - 특정 app의 urls.py 자체
+# {% url 'app_name:path_name' %}
+
+# RESTful- Rest의 구성요소
+# 1. 자원(Resource) - URI: 무엇을 가져올지
+# 2. 행위(Verb) - HTTP Method (GET, POST, PUT, DELETE, )
+# 3. 표현(Representation) - 자원 + 행위
+# ex)
+# GET    /users/1 =>user 1번 가져옴
+# PUT    /users/1
+# DELETE /users/1
--- django/crud_review/students/views.py
+++ django/crud_review/students/views.py
@@ -10,13 +10,23 @@ def index(request):
     return render(request, 'students/index.html', context)
 
 def new(request):
-    return render(request, 'students/new.html')
+    if request.method == 'POST':
+        # student를 생성(create)
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        student = Student(name=name, age=age)
+        student.save()
+        return redirect('students:detail', student.pk)
+    else: #=> 'GET'
+        # new page를 보여줌
+        return render(request, 'students/new.html')
 
-def create(request):
-    name = request.POST.get('name')
-    age = request.POST.get('age')
-    student = Student.objects.create(name=name, age=age)
-    return redirect(f'/students/{student.pk}/')
+# def create(request): => RESTful한 주소를 위해 new로 이동
+#     name = request.POST.get('name')
+#     age = request.POST.get('age')
+#     student = Student.objects.create(name=name, age=age)
+#     # return redirect(f'/students/{student.pk}/')
+#     return redirect('students:detail', student.pk)
 
 def detail(request, pk):
     student = Student.objects.get(id=pk)
@@ -26,26 +36,41 @@ def detail(request, pk):
     return render(request, 'students/detail.html', context)
 
 def delete(request, pk):
-    student = Student.objects.get(id=pk)
-    student.delete()
-    return redirect('/students/')
+    if request.method == 'POST': # GET요청이 들어올 경우 수행x
+        student = Student.objects.get(id=pk)
+        student.delete()
+        # return redirect('/students/')
+        return redirect('students:index')
 
 def edit(request, pk):
     student = Student.objects.get(id=pk)
-    context = {
-        'student': student,
-    }
-    return render(request, 'students/edit.html', context)
 
-def update(request, pk):
-    student = Student.objects.get(id=pk)
-    name = request.POST.get('name')
-    age = request.POST.get('age')
+    if request.method == 'POST':        
+        student = Student.objects.get(id=pk)
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        student.name = name
+        student.age = age
+        student.save()
+        return redirect('students:detail', student.pk)
 
-    student.name = name
-    student.age = age
-    student.save()
+    else:
+        context = {
+            'student': student,
+        }
+        return render(request, 'students/edit.html', context)
 
-    return redirect(f'/students/{student.pk}/')
+
+
+# def update(request, pk):
+#     student = Student.objects.get(id=pk)
+#     name = request.POST.get('name')
+#     age = request.POST.get('age')
+
+#     student.name = name
+#     student.age = age
+#     student.save()
+#     # return redirect(f'/students/{student.pk}/')
+#     return redirect('students:detail', student.pk)
 
 
